8cac6f60bc72f6c98103062ac7571defb59b55ef
LeandroCantero <lcantero18@gmail.com>
2023-05-12 00:02:54 -0300

    dark mode local storage

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ const body = document.querySelector('body');
 const navbar = document.getElementById('navbar')
 const form = document.getElementById('content')
 
-toggleBtn.addEventListener('click', function(){
+//Funciones modo oscuro y modo claro
+function lightMode() {
+    body.classList.remove('dark-mode')
+    navbar.classList.remove('dark-mode-navbar')
+    form.classList.remove('content-dark-mode')
+    localStorage.setItem('dark-mode', 'false')
+}
+
+function darkMode() {
+    body.classList.add('dark-mode')
+    navbar.classList.add('dark-mode-navbar')
+    form.classList.add('content-dark-mode')
+    localStorage.setItem('dark-mode', 'true')
+}
+
+//Cambia de modo claro a oscuro y/o viceversa
+toggleBtn.addEventListener('click', function () {
     this.classList.toggle('bi-emoji-sunglasses-fill');
-    if(this.classList.toggle('bi-emoji-sunglasses')){
-        body.classList.remove('dark-mode')
-        navbar.classList.remove('dark-mode-navbar')
-        content.classList.remove('content-dark-mode')
-    }else{
-        body.classList.add('dark-mode')
-        navbar.classList.add('dark-mode-navbar')
-        content.classList.add('content-dark-mode')
+    if (this.classList.toggle('bi-emoji-sunglasses')) {
+        lightMode()        
+    } else {
+        darkMode()
     }
 });
 
+
+
+//Obtengo el modo actual
+if (localStorage.getItem('dark-mode') === 'true') {
+    darkMode()
+    toggleBtn.classList.remove('bi-emoji-sunglasses')
+    toggleBtn.classList.add('bi-emoji-sunglasses-fill')
+} else {
+    lightMode()
+    toggleBtn.classList.remove('bi-emoji-sunglasses-fill')
+    toggleBtn.classList.add('bi-emoji-sunglasses')
+}
